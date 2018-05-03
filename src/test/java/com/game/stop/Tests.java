7639ae9b87e9f18a26bafcda8d0a2b1e5307197a
David Falcão <davidrsfalcao@gmail.com>
2018-05-03 12:44:51 +0100

    update

---
+++ src/test/java/com/game/stop/Tests.java
@@ -0,0 +1,14 @@
+package com.game.stop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+
+    @Test
+    public void testExample(){
+        int a = 1;
+        assertEquals(1,a);
+    }
+}
